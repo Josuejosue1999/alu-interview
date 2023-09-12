685d8b75d10aebaa61e7c16114abbe45858becfd
root <Josuejosue1999>
2023-09-12 14:08:51 +0000

    Task0

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 #Minimum Operations
 
-def minOperations(n: int) -> int:
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
+def min_operations_to_generate_h(n):
+    clipboard = 0  # Initialize clipboard to 0 H's
+    operations = 0  # Initialize the number of operations to 0
+
+    while clipboard < n:
+        if n % clipboard == 0:
+            clipboard = clipboard * 2  # Double the clipboard size
+            operations += 1
         else:
-            op += 1
-            body += next
-    if len(body) != n:
-        return 0
-    return op
+            clipboard += clipboard  # Copy all
+            operations += 1
+
+    return operations
+
+n = 9
+result = min_operations_to_generate_h(n)
+print("Number of operations:", result)

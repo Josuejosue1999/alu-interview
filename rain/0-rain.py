75ee43cae137615019abae8b076614f470fc761b
Josuejosue1999 <j.biaou@alustudent.com>
2023-10-08 09:48:31 +0200

    Done

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""square unit of water"""
-
+""" square unit of water to be retained after the rain rains"""
 
 def rain(walls):
-    """how many square units of water
+    """Calculate how many square units of water
     will be retained after it rains"""
     if len(walls) == 0:
         return 0
@@ -18,10 +17,8 @@ def rain(walls):
         water += min(left, right) - walls[i]
     return water
 
-
 if __name__ == "__main__":
     walls = [0, 1, 0, 2, 0, 3, 0, 4]
     print(rain(walls))
     walls = [2, 0, 0, 4, 0, 0, 1, 0]
     print(rain(walls))
-    
